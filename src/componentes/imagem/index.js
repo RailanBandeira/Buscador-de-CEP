2ea24e667d4f8b5d232e47023346fda1e07d9dda
Railan Andrade Bandeira <162853790+RailanBandeira@users.noreply.github.com>
2024-05-24 15:30:41 -0300

    concluido

---
+++ src/componentes/imagem/index.js
@@ -0,0 +1,11 @@
+import './cep.css'
+
+function Cep() {
+    return (
+        <header className="cep">
+            <img src='/imagens/cep.png'></img>
+        </header>
+    )
+}
+
+export default Cep
